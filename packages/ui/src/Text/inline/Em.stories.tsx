5475a9db44dd06eb4fd64b6b14c8857e6087d109
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-11-11 14:24:18 -0700

    add inline text elements
    These are replacements for the standard html elements but with the styled intended by the theme.

---
+++ packages/ui/src/Text/inline/Em.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Em, Text } from '../index'
+
+export default {
+  title: 'Components/Text/Inline',
+  component: Em,
+} as Meta<typeof Em>
+
+export const EmExample: StoryObj<typeof Em> = {
+  render: (args) => (
+    <Text>
+      You can use the <Em {...args}>{args.children}</Em>. Ullam veritatis in qui, commodi enim
+      earum obcaecati ipsam necessitatibus perferendis quas, mollitia est, sequi
+      quam. Excepturi!
+    </Text>
+  ),
+
+  args: {
+    children: '<Em> component to give text a strong emphasis',
+  },
+}
