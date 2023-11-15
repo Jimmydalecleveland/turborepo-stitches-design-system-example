7da5fab5d7e429aa7c6fffba6abe16fa1580e490
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-11-15 12:02:29 -0700

    add `Tag` component with stories and web app usage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,65 +1,75 @@
 import {
   Box,
   Button,
+  Card,
+  Check,
   Column,
   Columns,
   Heading,
   Inline,
   Rocket,
   Stack,
+  Tag,
   Text,
 } from "@jimmydalecleveland/stitches-ui-example";
 
 export default function Page(): React.ReactElement {
   return (
     <main>
-      <Stack space="06">
-        <Box vibe="neutral">
-          <Heading size="h1">Welcome to the Stitches Design System</Heading>
-          <Text as="p">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-            amet cum deserunt distinctio enim eos est excepturi expedita harum
-            id illum laudantium magni modi natus neque, nihil, nisi numquam odit
-            officia optio perspiciatis quia quo ratione rerum saepe veritatis
-            vero! Eum, illum quam. Dolores esse eum itaque repellat, vero
-            voluptatum.
-          </Text>
-        </Box>
-        <Inline space="05">
-          <Button vibe="neutral">Sign out</Button>
-          <Button RightIcon={Rocket} vibe="attract">
-            Sign up!
-          </Button>
-        </Inline>
+      <Box padding="04">
+        <Stack space="06">
+          <Box vibe="neutral">
+            <Heading size="h1">Welcome to the Stitches Design System</Heading>
+            <Text as="p">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Accusamus amet cum deserunt distinctio enim eos est excepturi
+              expedita harum id illum laudantium magni modi natus neque, nihil,
+              nisi numquam odit officia optio perspiciatis quia quo ratione
+              rerum saepe veritatis vero! Eum, illum quam. Dolores esse eum
+              itaque repellat, vero voluptatum.
+            </Text>
+          </Box>
+          <Card>
+            <Inline space="05">
+              <Tag LeftIcon={Check} vibe="positive">
+                Complete!
+              </Tag>
+              <Button vibe="neutral">Sign out</Button>
+              <Button RightIcon={Rocket} vibe="attract">
+                Sign up!
+              </Button>
+            </Inline>
+          </Card>
 
-        <Stack space="05">
-          <Heading size="h2">Heading 2</Heading>
-          <Text as="p">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-            ad asperiores culpa dolore eaque laudantium maxime molestias nobis
-            rem, vero.
-          </Text>
+          <Stack space="05">
+            <Heading size="h2">Heading 2</Heading>
+            <Text as="p">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Accusamus ad asperiores culpa dolore eaque laudantium maxime
+              molestias nobis rem, vero.
+            </Text>
+          </Stack>
+
+          <Columns space="04">
+            <Column>
+              <Heading as="h3">Heading 3</Heading>
+              <Text as="p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusamus ad asperiores culpa dolore eaque laudantium maxime
+                molestias nobis rem, vero.
+              </Text>
+            </Column>
+            <Column>
+              <Heading as="h3">Heading 3</Heading>
+              <Text as="p">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Accusamus ad asperiores culpa dolore eaque laudantium maxime
+                molestias nobis rem, vero.
+              </Text>
+            </Column>
+          </Columns>
         </Stack>
-
-        <Columns space="04">
-          <Column>
-            <Heading as="h3">Heading 3</Heading>
-            <Text as="p">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusamus ad asperiores culpa dolore eaque laudantium maxime
-              molestias nobis rem, vero.
-            </Text>
-          </Column>
-          <Column>
-            <Heading as="h3">Heading 3</Heading>
-            <Text as="p">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusamus ad asperiores culpa dolore eaque laudantium maxime
-              molestias nobis rem, vero.
-            </Text>
-          </Column>
-        </Columns>
-      </Stack>
+      </Box>
     </main>
   );
 }
---
+++ packages/ui/src/Tag/Tag.stories.tsx
@@ -0,0 +1,86 @@
+import type { Meta } from "@storybook/react";
+import { Stack } from "../Stack";
+import { Check, Download, XMark } from "../Icon";
+import { Box } from "../Box";
+import { Tag } from ".";
+
+export default {
+  title: "Components/Tag",
+  component: Tag,
+} satisfies Meta<typeof Tag>;
+
+export const Default = {
+  args: {
+    children: "Label",
+  },
+};
+
+export const WithIcon = {
+  args: {
+    children: "Label",
+    vibe: "critical",
+    RightIcon: XMark,
+  },
+};
+
+export const WithBothIcons = {
+  args: {
+    children: "Label",
+    vibe: "positive",
+    LeftIcon: Check,
+    RightIcon: Download,
+  },
+};
+
+export const WithOnlyIcon = {
+  args: {
+    vibe: "critical",
+    LeftIcon: XMark,
+  },
+};
+
+export const AllVibes = {
+  render: () => (
+    <Stack space="05">
+      <Tag RightIcon={XMark} vibe="neutral">
+        Neutral
+      </Tag>
+      <Tag RightIcon={XMark} vibe="neutralInverse">
+        Neutral Inverse
+      </Tag>
+      <Tag RightIcon={XMark} vibe="disabled">
+        Disabled
+      </Tag>
+      <Tag RightIcon={XMark} vibe="attract">
+        Attract
+      </Tag>
+      <Tag RightIcon={Check} vibe="positive">
+        Positive
+      </Tag>
+      <Tag RightIcon={Download} vibe="warning">
+        Warning
+      </Tag>
+      <Tag RightIcon={XMark} vibe="critical">
+        Critical
+      </Tag>
+      <Tag RightIcon={Download} vibe="info">
+        Info
+      </Tag>
+      <Box
+        display="inlineBlock"
+        vibe="neutral"
+        padding="03"
+        shadow="low"
+        corners="rounder"
+      >
+        <Tag vibe="subdued" RightIcon={Download}>
+          Subdued
+        </Tag>
+      </Box>
+    </Stack>
+  ),
+
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+};
