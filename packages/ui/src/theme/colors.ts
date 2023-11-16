22d89cdf39e62ef80336e8677b7ad67cd78d467f
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-11-16 15:16:51 -0700

    theme: update entire theme colors
    The previous theme was just a placeholder, and this one has some more time put into it. Still some edges to smooth out, but it is a decent start.

--- packages/ui/src/theme/colors.ts
+++ packages/ui/src/theme/colors.ts
@@ -22,16 +22,16 @@ type SupportColorArray = readonly [string, string, string, string, string];
  * can define the overall feel and can elicit emotion.
  */
 export const primary: FullColorArray = [
-  "hsla(330, 100%, 95%, 1)", // Light Pink
-  "hsla(330, 100%, 85%, 1)", // Soft Pink
-  "hsla(330, 100%, 75%, 1)", // Bright Pink
-  "hsla(330, 100%, 65%, 1)", // Strong Pink
-  "hsla(330, 100%, 55%, 1)", // Deep Pink
-  "hsla(330, 100%, 45%, 1)", // Dark Pink
-  "hsla(330, 100%, 35%, 1)", // Deeper Pink
-  "hsla(330, 100%, 25%, 1)", // Very Dark Pink
-  "hsla(330, 100%, 15%, 1)", // Almost Black Pink
-  "hsla(330, 100%, 5%, 1)", // Near Black
+  "hsl(210, 36%, 96%)",
+  "hsl(212, 33%, 89%)",
+  "hsl(210, 31%, 80%)",
+  "hsl(211, 27%, 70%)",
+  "hsl(209, 23%, 60%)",
+  "hsl(210, 22%, 49%)",
+  "hsl(209, 28%, 39%)",
+  "hsl(209, 34%, 30%)",
+  "hsl(211, 39%, 23%)",
+  "hsl(209, 61%, 16%)",
 ];
 
 /*
@@ -40,49 +40,50 @@ export const primary: FullColorArray = [
  * all interactive elements such as buttons, links, inputs, etc.
  */
 export const secondary: FullColorArray = [
-  "hsla(260, 100%, 95%, 1)", // Light Purple
-  "hsla(260, 100%, 85%, 1)", // Soft Purple
-  "hsla(260, 100%, 75%, 1)", // Bright Purple
-  "hsla(260, 100%, 65%, 1)", // Strong Purple
-  "hsla(260, 100%, 55%, 1)", // Deep Purple
-  "hsla(260, 100%, 45%, 1)", // Dark Purple
-  "hsla(260, 100%, 35%, 1)", // Deeper Purple
-  "hsla(260, 100%, 25%, 1)", // Very Dark Purple
-  "hsla(260, 100%, 15%, 1)", // Almost Black Purple
-  "hsla(260, 100%, 5%, 1)", // Near Black
+  "hsl(152, 68%, 96%)",
+  "hsl(154, 75%, 87%)",
+  "hsl(156, 73%, 74%)",
+  "hsl(158, 58%, 62%)",
+  "hsl(160, 51%, 49%)",
+  "hsl(162, 63%, 41%)",
+  "hsl(164, 71%, 34%)",
+  "hsl(166, 72%, 28%)",
+  "hsl(168, 80%, 23%)",
+  "hsl(170, 97%, 15%)",
 ];
+
 /*
  * The tertiary color highlights or complements the
  * primary and secondary color. These are to be used
  * to make the UI elements stand out.
  */
 export const tertiary: FullColorArray = [
-  "hsla(190, 100%, 95%, 1)", // Light Blue
-  "hsla(190, 100%, 85%, 1)", // Soft Blue
-  "hsla(190, 100%, 75%, 1)", // Bright Blue
-  "hsla(190, 100%, 65%, 1)", // Strong Blue
-  "hsla(190, 100%, 55%, 1)", // Deep Blue
-  "hsla(190, 100%, 45%, 1)", // Dark Blue
-  "hsla(190, 100%, 35%, 1)", // Deeper Blue
-  "hsla(190, 100%, 25%, 1)", // Very Dark Blue
-  "hsla(190, 100%, 15%, 1)", // Almost Black Blue
-  "hsla(190, 100%, 5%, 1)", // Near Black
+  "hsl(152, 22%, 96%)",
+  "hsl(154, 18%, 87%)",
+  "hsl(156, 14%, 74%)",
+  "hsl(158, 10%, 62%)",
+  "hsl(160, 08%, 49%)",
+  "hsl(162, 10%, 41%)",
+  "hsl(164, 14%, 34%)",
+  "hsl(166, 18%, 28%)",
+  "hsl(168, 22%, 23%)",
+  "hsl(170, 26%, 15%)",
 ];
 
 /*
  * This is color is used for star ratings.
  */
 export const extended: FullColorArray = [
-  "hsla(45, 100%, 95%, 1)", // Light Yellow
-  "hsla(45, 100%, 85%, 1)", // Soft Yellow
-  "hsla(45, 100%, 75%, 1)", // Bright Yellow
-  "hsla(45, 100%, 65%, 1)", // Strong Yellow
-  "hsla(45, 100%, 55%, 1)", // Deep Yellow
-  "hsla(45, 100%, 45%, 1)", // Dark Yellow
-  "hsla(45, 100%, 35%, 1)", // Deeper Yellow
-  "hsla(45, 100%, 25%, 1)", // Very Dark Yellow
-  "hsla(45, 100%, 15%, 1)", // Almost Black Yellow
-  "hsla(45, 100%, 5%, 1)", // Near Black
+  "hsla(30, 86%, 93%, 1)",
+  "hsla(38, 100%, 83%, 1)",
+  "hsla(32, 100%, 73%, 1)",
+  "hsla(32, 100%, 65%, 1)",
+  "hsla(30, 86%, 62%, 1)",
+  "hsla(30, 86%, 49%, 1)",
+  "hsla(30, 86%, 42%, 1)",
+  "hsla(30, 86%, 33%, 1)",
+  "hsla(30, 87%, 22%, 1)",
+  "hsla(29, 88%, 11%, 1)",
 ];
 
 /*
@@ -90,16 +91,16 @@ export const extended: FullColorArray = [
  * Think background, form fields, dividers, etc.
  */
 export const gray: FullColorArray = [
-  "hsla(210, 15%, 96%, 1)", // Very Light Gray
-  "hsla(210, 15%, 88%, 1)", // Light Gray
-  "hsla(210, 10%, 78%, 1)", // Soft Gray
-  "hsla(210, 10%, 68%, 1)", // Medium Gray
-  "hsla(210, 10%, 58%, 1)", // Neutral Gray
-  "hsla(210, 10%, 48%, 1)", // Dark Gray
-  "hsla(210, 15%, 38%, 1)", // Darker Gray
-  "hsla(210, 15%, 28%, 1)", // Very Dark Gray
-  "hsla(210, 20%, 18%, 1)", // Almost Black
-  "hsla(210, 20%, 8%, 1)", // Near Black
+  "hsl(216, 33%, 97%)",
+  "hsl(214, 15%, 91%)",
+  "hsl(210, 16%, 82%)",
+  "hsl(211, 13%, 65%)",
+  "hsl(211, 10%, 53%)",
+  "hsl(211, 12%, 43%)",
+  "hsl(209, 14%, 37%)",
+  "hsl(209, 18%, 30%)",
+  "hsl(209, 20%, 25%)",
+  "hsl(210, 24%, 16%)",
 ];
 
 /*
@@ -107,16 +108,16 @@ export const gray: FullColorArray = [
  * vary in intensity depending on the context, such as headlines and bold callouts.
  */
 export const text: FullColorArray = [
-  "hsla(0, 0%, 100%, 0.9)", // Almost White
-  "hsla(0, 0%, 95%, 1)", // Very Light Text
-  "hsla(0, 0%, 85%, 1)", // Light Text
-  "hsla(0, 0%, 75%, 1)", // Soft Text
-  "hsla(0, 0%, 65%, 1)", // Regular Text
-  "hsla(0, 0%, 55%, 1)", // Medium Text
-  "hsla(0, 0%, 45%, 1)", // Dark Text
-  "hsla(0, 0%, 35%, 1)", // Darker Text
-  "hsla(0, 0%, 25%, 1)", // Very Dark Text
-  "hsla(0, 0%, 15%, 1)", // Almost Black Text
+  "hsl(180, 20%, 97%)",
+  "hsl(180, 14%, 91%)",
+  "hsl(180, 8%, 82%)",
+  "hsl(180, 5%, 65%)",
+  "hsl(180, 3%, 53%)",
+  "hsl(180, 5%, 43%)",
+  "hsl(180, 6%, 37%)",
+  "hsl(180, 8%, 30%)",
+  "hsl(180, 14%, 25%)",
+  "hsl(180, 20%, 16%)",
 ];
 
 /*
@@ -124,33 +125,33 @@ export const text: FullColorArray = [
  * These are used for alerts, banners, form validation, etc.
  */
 export const positive: SupportColorArray = [
-  "hsla(145, 60%, 90%, 1)", // Light Green
-  "hsla(145, 60%, 75%, 1)", // Soft Green
-  "hsla(145, 70%, 55%, 1)", // Vibrant Green
-  "hsla(145, 80%, 40%, 1)", // Dark Green
-  "hsla(145, 90%, 30%, 1)", // Deep Green
+  "hsla(152, 68%, 96%, 1)",
+  "hsla(156, 73%, 74%, 1)",
+  "hsla(160, 51%, 49%, 1)",
+  "hsla(164, 71%, 34%, 1)",
+  "hsla(170, 97%, 15%, 1)",
 ];
 
 export const critical: SupportColorArray = [
-  "hsla(360, 80%, 90%, 1)", // Light Red
-  "hsla(360, 80%, 75%, 1)", // Soft Red
-  "hsla(360, 90%, 60%, 1)", // Bright Red
-  "hsla(360, 100%, 40%, 1)", // Dark Red
-  "hsla(360, 100%, 25%, 1)", // Deep Red
+  "hsla(360, 100%, 95%, 1)",
+  "hsla(360, 100%, 80%, 1)",
+  "hsla(356, 75%, 53%, 1)",
+  "hsla(352, 90%, 35%, 1)",
+  "hsla(348, 94%, 20%, 1)",
 ];
 
 export const warning: SupportColorArray = [
-  "hsla(30, 90%, 90%, 1)", // Light Orange
-  "hsla(30, 90%, 75%, 1)", // Soft Orange
-  "hsla(30, 100%, 60%, 1)", // Bright Orange
-  "hsla(30, 100%, 45%, 1)", // Dark Orange
-  "hsla(30, 100%, 30%, 1)", // Deep Orange
+  "hsla(58, 94%, 91%, 1)",
+  "hsla(54, 100%, 69%, 1)",
+  "hsla(50, 94%, 50%, 1)",
+  "hsla(45, 100%, 40%, 1)",
+  "hsla(40, 100%, 26%, 1)",
 ];
 
 export const info: SupportColorArray = [
-  "hsla(210, 90%, 90%, 1)", // Light Blue
-  "hsla(210, 90%, 75%, 1)", // Soft Blue
-  "hsla(210, 100%, 60%, 1)", // Bright Blue
-  "hsla(210, 100%, 45%, 1)", // Dark Blue
-  "hsla(210, 100%, 30%, 1)", // Deep Blue
+  "hsla(180, 100%, 92%, 1)",
+  "hsla(192, 100%, 79%, 1)",
+  "hsla(192, 100%, 45%, 1)",
+  "hsla(194, 100%, 37%, 1)",
+  "hsla(199, 100%, 25%, 1)",
 ];
